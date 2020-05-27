23de6209ae8c030323569546f62edd68130bc522
Daniel Gratz <gratz.dhg@gmail.com>
2020-05-27 18:00:38 -0400

    parse_cmd_args max time is now an int

--- parse_cmd_args.py
+++ parse_cmd_args.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(prog='Program to Fit Ion Channels')
 parser.add_argument('program_location')
 parser.add_argument('--data_root', nargs='?', default='C:/Users/grat05/OneDrive for Business/Data')
 parser.add_argument('--out_dir', nargs='?', default='./')
-parser.add_argument('--max_time', nargs='?', help='Max time allowed for MCMC sampler (hours)')
+parser.add_argument('--max_time', nargs='?', type=float, help='Max time allowed for MCMC sampler (hours)')
 parser.add_argument('--previous_run', nargs='?')
 parser.add_argument('--model_name', nargs='?', default='Koval')
 
